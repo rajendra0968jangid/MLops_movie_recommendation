9aaf28435e58fe648696a01553bd0032461ac6be
rajendra0968jangid <rajendra0968jangid@gmail.com>
2026-09-01 22:52:33 +0530

    code

---
+++ frontend/Dockerfile
@@ -0,0 +1,3 @@
+FROM nginx:alpine
+COPY index.html style.css script.js /usr/share/nginx/html/
+EXPOSE 80
--- frontend/script.js
+++ frontend/script.js
@@ -32,7 +32,7 @@ async function getRecommendations() {
 
   try {
     // Fetch recommendations directly from your Flask pipeline
-    const response = await fetch("http://127.0.0.1:5000/recommend", {
+    const response = await fetch("http://localhost:5001/recommend", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ movie_title: movieTitle }),
